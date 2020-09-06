21188560d9c04d7ab3001e46b92955278884447d
Julien PERUGINI <julien.perugini@bred-it.com>
2020-09-06 12:36:06 +0700

    Solution palindrome

--- palindrome-number/src/main/java/fr/bred/Solution.java
+++ palindrome-number/src/main/java/fr/bred/Solution.java
@@ -4,7 +4,12 @@ public class Solution
 {
     public boolean solve( int number )
     {
-        // Your solution code goes here
-        return false;
+        if(number < 0) return false;
+        return revert(number, 0) == number;
+    }
+
+    private int revert(int number, int res) {
+        if(number<1) return res;
+        return revert(number / 10, res * 10 + number % 10);
     }
 }
