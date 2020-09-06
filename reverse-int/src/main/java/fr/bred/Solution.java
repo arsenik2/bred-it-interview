cd7bf3c75b57b346dbcb952cd9d3947d1cefe00f
Julien PERUGINI <julien.perugini@bred-it.com>
2020-09-06 11:57:41 +0700

    Solution reverse int

--- reverse-int/src/main/java/fr/bred/Solution.java
+++ reverse-int/src/main/java/fr/bred/Solution.java
@@ -4,7 +4,12 @@ public class Solution
 {
     public int solve( int number )
     {
-        // Your solution code goes here
-        return 0;
+        if(number == 0) return 0;
+        return revert(Math.abs(number), 0)*(Math.abs(number) / number);
+    }
+
+    public int revert(int number, int res) {
+        if(number<1) return res;
+        return revert(number / 10, res * 10 + number % 10);
     }
 }
